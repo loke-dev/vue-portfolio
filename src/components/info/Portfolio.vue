cde467ba6232051e55f9045ad0af3ac1c3f59da9
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-10-20 21:38:02 +0200

    Updated portfolio with new projects

--- src/components/info/Portfolio.vue
+++ src/components/info/Portfolio.vue
@@ -1,20 +1,7 @@
 <template>
   <div id='portfolio'>
-    <q-card inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/jekyll.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Buyone
-        <span slot="subtitle">E-commerce, built with Magento 2</span>
-      </q-card-title>
-      <q-card-actions>
-        <a href="https://lokecarlsson.github.io"><q-btn color="primary">Demo</q-btn></a>
-        <a href="https://github.com/LokeCarlsson/lokecarlsson.github.io"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
-
+    
+    
     <q-card inline>
       <q-card-media>
         <img class="responsive" src="../../statics/buyone.png">
@@ -30,6 +17,50 @@
       </q-card-actions>
     </q-card>
 
+    <q-card inline>
+      <q-card-media>
+        <img class="responsive" src="../../statics/pwd.png">
+      </q-card-media>
+      <q-card-separator />
+      <q-card-title>
+        Personal Web Desktop
+        <span slot="subtitle">A single page application designed to imitate macOS</span>
+      </q-card-title>
+      <q-card-actions>
+        <a disabled><q-btn color="primary">Demo</q-btn></a>
+        <a href="https://github.com/LokeCarlsson/PWD"><q-btn color="primary">Github</q-btn></a>
+      </q-card-actions>
+    </q-card>
+
+    <q-card inline>
+      <q-card-media>
+        <img class="responsive" src="../../statics/jekyll.png">
+      </q-card-media>
+      <q-card-separator />
+      <q-card-title>
+        Jekyll
+        <span slot="subtitle">Github pages built with jekyll</span>
+      </q-card-title>
+      <q-card-actions>
+        <a href="https://lokecarlsson.github.io"><q-btn color="primary">Demo</q-btn></a>
+        <a href="https://github.com/LokeCarlsson/lokecarlsson.github.io"><q-btn color="primary">Github</q-btn></a>
+      </q-card-actions>
+    </q-card>
+
+    <q-card inline>
+      <q-card-media>
+        <img class="responsive" src="../../statics/carleudd.png">
+      </q-card-media>
+      <q-card-separator />
+      <q-card-title>
+        Carleudd
+        <span slot="subtitle">Simple website built with HTML5 and CSS3</span>
+      </q-card-title>
+      <q-card-actions>
+        <a disabled><q-btn color="primary">Demo</q-btn></a>
+        <a href="https://github.com/LokeCarlsson/lokecarlsson.github.io"><q-btn color="primary">Github</q-btn></a>
+      </q-card-actions>
+    </q-card>
 
     <h1>More to come...</h1>
   </div>
