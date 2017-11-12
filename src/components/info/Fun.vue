9278a4eab85fba2ad0106eaf7cc91ef4a64671f1
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-11-12 23:13:43 +0100

    Added fun component

---
+++ src/components/info/Fun.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id='fun'>
+			<q-tabs>
+				<q-route-tab
+					icon="mail"
+					to="/mails"
+					exact
+					slot="title"    
+				/>
+				<q-route-tab
+					icon="alarm"
+					to="/alarms"
+					exact
+					slot="title"
+				/>
+			</q-tabs>
+  </div>
+</template>
+
+<script>
+import {
+  QTabs,
+  QTab,
+  QOptionGroup
+} from 'quasar'
+export default {
+  components: {
+    QTabs,
+    QTab,
+    QOptionGroup
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
