a239baa5876335b283dde8275562bed4421302e4
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-11-14 22:54:40 +0100

    Added shop part of cookie clicker game

---
+++ src/components/apps/Shop.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="shop">
+    <q-card class="card" key="card" inline>
+        <q-card-title>
+          {{ producer.name }}
+          <span slot="subtitle">
+            +{{ producer.cookiePerSeconds.toLocaleString('sv-SE', { maximumFractionDigits: 2 }) }} cookies/s
+          </span>
+        </q-card-title>
+        <div class="row">
+          <div class="col-8 price">
+            <img class="icon" src="../../statics/svg/cookie.svg" alt="">
+            <div :class="{'text-green': canBuy === true, 'text-red': canBuy === false }">{{ producer.price.toLocaleString('sv-SE', { maximumFractionDigits: 0 }) }}</div>
+          </div>
+          <div class="col-4 count" ref="count">
+            <span>{{ producer.purchased }}</span>
+          </div>
+        </div>
+        <q-card-actions>
+          <q-btn @click="buy" class="buyButton" color="primary">Buy</q-btn>
+        </q-card-actions>
+      </q-card>
+  </div>
+</template>
+
+<script>
+import Store from '../../vuex/store'
+import {
+  QCard,
+  QCardTitle,
+  QCardMedia,
+  QCardActions,
+  QBtn,
+  QCardSeparator
+} from 'quasar'
+export default {
+  components: {
+    Store,
+    QCard,
+    QCardTitle,
+    QCardMedia,
+    QCardActions,
+    QBtn,
+    QCardSeparator
+  },
+  props: ['producer'],
+  computed: {
+    totalCookies: () => Store.state.cookie.cookies,
+    canBuy: function () { return this.totalCookies >= this.producer.price }
+  },
+  methods: {
+    buy () {
+      if (this.canBuy) {
+        Store.commit('BUY_PRODUCER', { producerId: this.producer.id })
+        Store.commit('COUNT_COOKIES')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.card
+  min-width: 300px
+
+.buyButton
+  width: 80px
+  margin: 0 auto
+
+.shop
+  user-select: none
+
+.price
+  display: flex
+
+.count
+  font-size: 32px
+  opacity: 0.3
+  padding-left: 25px
+
+.icon
+  width: 20px
+  height: 20px
+  margin-right: 5px
+  margin-left: 10px
+
+.text-green
+  color: green
+
+.text-red
+  color: red
+</style>
---
+++ src/components/apps/ShopSection.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class='shop-container'>
+        <div class='shop-list'>
+            <Shop v-for='producer in producers' :producer='producer' :key='producer.name' />
+        </div>
+    </section>
+</template>
+
+<script>
+import Store from '../../vuex/store'
+import Shop from './Shop'
+export default {
+  components: {
+    Store,
+    Shop
+  },
+  computed: {
+    producers: () => Store ? Store.state.cookie.producers : null
+  }
+}
+</script>
+
+
+<style scoped lang="stylus">
+.shop-container 
+  cursor: default
+</style>
