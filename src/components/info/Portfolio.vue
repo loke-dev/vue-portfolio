176573321b25d209d5fc942dd4dae2119347988d
LokeCarlsson <loke.carlsson.lc@gmail.com>
2017-11-14 22:56:40 +0100

    Adapted URLs to reflect changes to project structure

--- src/components/info/Portfolio.vue
+++ src/components/info/Portfolio.vue
@@ -6,128 +6,128 @@
       enter="slideInUp"
       leave="SlideInDown"
     >
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/gitgud.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Gitgud
+          <span slot="subtitle">Github dashboard</span>
+        </q-card-title>
+        <q-card-actions>
+          <a target="_blank" href="http://gitgud.nu/"><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/Gitgud"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+        <q-inner-loading :visible="visible">
+          <q-spinner-gears size="50px" color="primary"></q-spinner-gears>
+        </q-inner-loading>
+      </q-card>
+      
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/buyone.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Buyone
+          <span slot="subtitle">E-commerce, built with Magento 2</span>
+        </q-card-title>
+        <q-card-actions>
+          <a><q-btn disable color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/Buyone"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
 
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/gitgud.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Gitgud
-        <span slot="subtitle">Github dashboard</span>
-      </q-card-title>
-      <q-card-actions>
-        <a target="_blank" href="http://gitgud.nu/"><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/Gitgud"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
-    
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/buyone.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Buyone
-        <span slot="subtitle">E-commerce, built with Magento 2</span>
-      </q-card-title>
-      <q-card-actions>
-        <a><q-btn disable color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/Buyone"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/pwd.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Personal Web Desktop
+          <span slot="subtitle">SPA macOS clone</span>
+        </q-card-title>
+        <q-card-actions>
+          <a target="_blank" href="http://pwd.lokecarlsson.se/"><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/PWD"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
 
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/pwd.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Personal Web Desktop
-        <span slot="subtitle">SPA macOS clone</span>
-      </q-card-title>
-      <q-card-actions>
-        <a target="_blank" href="http://pwd.lokecarlsson.se/"><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/PWD"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/snippet.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Snippets
+          <span slot="subtitle">Web application for snippet management</span>
+        </q-card-title>
+        <q-card-actions>
+          <a disabled><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/Snippets"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
 
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/snippet.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Snippets
-        <span slot="subtitle">Web application for snippet management</span>
-      </q-card-title>
-      <q-card-actions>
-        <a disabled><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/Snippets"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/quiz.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          QuizMaster
+          <span slot="subtitle">Javascript quiz API</span>
+        </q-card-title>
+        <q-card-actions>
+          <a target="_blank" disabled><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/PWD"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
 
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/quiz.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        QuizMaster
-        <span slot="subtitle">Javascript quiz API</span>
-      </q-card-title>
-      <q-card-actions>
-        <a target="_blank" disabled><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/PWD"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/jekyll.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Jekyll
+          <span slot="subtitle">Github pages built with jekyll</span>
+        </q-card-title>
+        <q-card-actions>
+          <a target="_blank" href="https://lokecarlsson.github.io"><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/lokecarlsson.github.io"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
 
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/jekyll.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Jekyll
-        <span slot="subtitle">Github pages built with jekyll</span>
-      </q-card-title>
-      <q-card-actions>
-        <a target="_blank" href="https://lokecarlsson.github.io"><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/lokecarlsson.github.io"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/lillafro.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Lilla frö
+          <span slot="subtitle">Presentation website/landingpage</span>
+        </q-card-title>
+        <q-card-actions>
+          <a target="_blank" href="http://lillafrö.nu"><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/lillafro"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
 
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/lillafro.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Lilla frö
-        <span slot="subtitle">Presentation website/landingpage</span>
-      </q-card-title>
-      <q-card-actions>
-        <a target="_blank" href="http://lillafrö.nu"><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/lillafro"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
-
-    <q-card key="card" inline>
-      <q-card-media>
-        <img class="responsive" src="../../statics/carleudd.png">
-      </q-card-media>
-      <q-card-separator />
-      <q-card-title>
-        Carleudd
-        <span slot="subtitle">Simple website built with HTML5 and CSS3</span>
-      </q-card-title>
-      <q-card-actions>
-        <a target="_blank" href="http://carleudd.lokecarlsson.se"><q-btn color="primary">Demo</q-btn></a>
-        <a target="_blank" href="https://github.com/LokeCarlsson/carleudd"><q-btn color="primary">Github</q-btn></a>
-      </q-card-actions>
-    </q-card>
-
-    <h1 key="text">More to come...</h1>
+      <q-card key="card" inline>
+        <q-card-media>
+          <img class="responsive" src="../../statics/projects/carleudd.png">
+        </q-card-media>
+        <q-card-separator />
+        <q-card-title>
+          Carleudd
+          <span slot="subtitle">Simple website built with HTML5 and CSS3</span>
+        </q-card-title>
+        <q-card-actions>
+          <a target="_blank" href="http://carleudd.lokecarlsson.se"><q-btn color="primary">Demo</q-btn></a>
+          <a target="_blank" href="https://github.com/LokeCarlsson/carleudd"><q-btn color="primary">Github</q-btn></a>
+        </q-card-actions>
+      </q-card>
     </q-transition>
   </div>
 </template>
@@ -140,7 +140,9 @@ import {
   QCardActions,
   QBtn,
   QCardSeparator,
-  QTransition
+  QTransition,
+  QInnerLoading,
+  QSpinnerGears
 } from 'quasar'
 
 export default {
@@ -151,7 +153,9 @@ export default {
     QCardActions,
     QBtn,
     QCardSeparator,
-    QTransition
+    QTransition,
+    QInnerLoading,
+    QSpinnerGears
   },
   data () {
     return {
@@ -165,7 +169,6 @@ export default {
   max-width 300px
   min-height 434px
   margin 20px
-    
 a
   margin-left 5px
   margin-right 5px
