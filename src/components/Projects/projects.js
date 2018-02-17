56b7f2e2ccfeb9d4063a333b517620ee97f1b6de
LokeCarlsson <loke.carlsson.lc@gmail.com>
2018-02-17 22:15:02 +0100

    Added rest of projects

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -2,8 +2,19 @@ import * as images from '../../assets/img/projects';
 
 export default [
   {
+    name: 'Gitgud',
+    year: '2016',
+    url: 'http://gitgud.lokecarlsson.se/',
+    repo: 'https://github.com/LokeCarlsson/Gitgud',
+    projectImage: images.gitgud,
+    tags: ['Vue', 'Vuex', 'Quasar', 'Express', 'Passport', 'Mongoose', 'Socket.io'],
+    description: `
+      Gitgud is a Github Dashboard built with Vue frontend and Express API.
+    `,
+  },
+  {
     name: 'Buyone',
-    date: new Date(2015),
+    year: '2015',
     url: null,
     repo: 'https://github.com/LokeCarlsson/Buyone',
     projectImage: images.buyone,
@@ -13,36 +24,70 @@ export default [
     `,
   },
   {
+    name: 'Lilla Frö',
+    year: '2017',
+    url: 'https://xn--lillafr-g1a.nu/',
+    repo: 'https://github.com/LokeCarlsson/lillafro',
+    projectImage: images.lillafro,
+    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
+    description: `
+      Buyone is a e-commerce website build as a boilerplate project.
+    `,
+  },
+  {
+    name: 'Jekyll portfolio',
+    year: '2015',
+    url: 'https://about.gitlab.com/features/#ee-premium',
+    repo: 'https://gitlab.com/gitlab-org/gitlab-ee',
+    projectImage: images.jekyll,
+    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
+    description: `
+      Buyone is a e-commerce website build as a boilerplate project.
+    `,
+  },
+  {
+    name: 'PWD',
+    year: '2015',
+    url: 'https://about.gitlab.com/features/#ee-premium',
+    repo: 'https://gitlab.com/gitlab-org/gitlab-ee',
+    projectImage: images.pwd,
+    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
+    description: `
+      Buyone is a e-commerce website build as a boilerplate project.
+    `,
+  },
+  {
+    name: 'Snippet',
+    year: '2015',
+    url: 'https://about.gitlab.com/features/#ee-premium',
+    repo: 'https://gitlab.com/gitlab-org/gitlab-ee',
+    projectImage: images.snippet,
+    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
+    description: `
+      Buyone is a e-commerce website build as a boilerplate project.
+    `,
+  },
+  {
     name: 'Carleudd',
-    date: new Date(2015),
+    year: '2015',
     url: 'https://about.gitlab.com/features/#ee-premium',
     repo: 'https://gitlab.com/gitlab-org/gitlab-ee',
     projectImage: images.carleudd,
-    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
+    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate', 'lol', 'hej'],
     description: `
       Buyone is a e-commerce website build as a boilerplate project.
     `,
   },
   {
-    name: 'Gitgud',
-    date: new Date(2015),
+    name: 'Quiz',
+    year: '2015',
     url: 'https://about.gitlab.com/features/#ee-premium',
     repo: 'https://gitlab.com/gitlab-org/gitlab-ee',
-    projectImage: images.gitgud,
+    projectImage: images.quiz,
     tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
     description: `
       Buyone is a e-commerce website build as a boilerplate project.
     `,
   },
-  {
-    name: 'Buyone',
-    date: new Date(2015),
-    url: 'https://about.gitlab.com/features/#ee-premium',
-    repo: 'https://gitlab.com/gitlab-org/gitlab-ee',
-    projectImage: images.buyone,
-    tags: ['PHP', 'E-commerce', 'Magento', 'Boilerplate'],
-    description: `
-      Buyone is a e-commerce website build as a boilerplate project.
-    `,
-  },
+
 ];
