60e6a57ffd0046dc0448748be53084b131f611c0
LokeCarlsson <loke.carlsson.lc@gmail.com>
2018-02-17 18:51:36 +0100

    Added exporter for project images

---
+++ src/assets/img/projects/index.js
@@ -0,0 +1,8 @@
+export { default as buyone } from './buyone.png';
+export { default as carleudd } from './carleudd.png';
+export { default as gitgud } from './gitgud.png';
+export { default as jekyll } from './jekyll.png';
+export { default as lillafro } from './lillafro.png';
+export { default as pwd } from './pwd.png';
+export { default as quiz } from './quiz.png';
+export { default as snippet } from './snippet.png';
