57dd3eb092f0c94e54b53e61abab5cf40aa8a6cf
LokeCarlsson <loke.carlsson.lc@gmail.com>
2018-02-17 18:53:52 +0100

    Removed unused code and components

--- src/components/Projects/index.vue
+++ src/components/Projects/index.vue
@@ -4,46 +4,14 @@
       <h2 class="title is-2">Projects</h2>
       <separator></separator>
     </div>
-  <!-- <div class="cardContainer">
-    <Card data-image="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=">
-      <h1 slot="header">Canyons</h1>
-      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Card>
-
-    <Card data-image="https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=">
-      <h1 slot="header">Canyons</h1>
-      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Card>
-
-    <Card data-image="https://images.unsplash.com/photo-1479659929431-4342107adfc1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=">
-      <h1 slot="header">Beaches</h1>
-      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Card>
-
-    <Card data-image="https://images.unsplash.com/photo-1479644025832-60dabb8be2a1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=">
-      <h1 slot="header">Trees</h1>
-      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Card>
-    
-    <Card data-image="https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop=">
-      <h1 slot="header">Lakes</h1>
-      <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Card>
-  </div> -->
-
     <project-list class="cardContainer" :projects="projects"></project-list>
-
-  
   </section>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 
-import RepoPanel from './RepoPanel';
 import ProjectList from './ProjectList';
-import ProjectDetails from './ProjectDetails';
-import Timeline from './Timeline';
 import Card from './Card';
 
 import projects from './projects';
@@ -51,10 +19,7 @@ import projects from './projects';
 export default {
   name: 'projects',
   components: {
-    RepoPanel,
     ProjectList,
-    ProjectDetails,
-    Timeline,
     Card,
   },
   data() {
