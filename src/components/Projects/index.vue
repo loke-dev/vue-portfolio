56c382cae8b7906500c9e19cd00e8e1fb6723626
LokeCarlsson <loke.carlsson.lc@gmail.com>
2018-02-17 22:15:14 +0100

    Fixed project cards

--- src/components/Projects/index.vue
+++ src/components/Projects/index.vue
@@ -4,41 +4,34 @@
       <h2 class="title is-2">Projects</h2>
       <separator></separator>
     </div>
-    <project-list class="cardContainer" :projects="projects"></project-list>
+    <div class="cardContainer columns">
+      <div
+        class="project tile column is-3"
+        v-for="(project, index) in projects"
+        :key="index">
+        <Card class="reveal" :data-project="project">
+        </Card>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
-import ProjectList from './ProjectList';
 import Card from './Card';
-
 import projects from './projects';
 
 export default {
   name: 'projects',
   components: {
-    ProjectList,
     Card,
+  },
+  mounted() {
+    sr.reveal('#projects .reveal', 200);
   },
   data() {
     return {
       projects,
-      mode: 'Designer',
     };
-  },
-  computed: {
-    ...mapState([
-      'activeProject',
-      'showDetails',
-    ]),
-    career() {
-      return projects.filter(p => p.type === 'career');
-    },
-    personal() {
-      return projects.filter(p => p.type === 'personal');
-    },
   },
 };
 </script>
@@ -46,6 +39,47 @@ export default {
 <style lang="scss" scoped>
 @import '~vars';
 @import '~bulma/sass/utilities/mixins';
+
+.project-list {
+  padding: 1.5rem 0;
+
+  @include mobile {
+      padding: 1.5rem 1rem;
+  }
+
+  .project {
+    padding: 0.5rem 1rem;
+
+    a {
+      position: relative;
+      color: rgba($white, 0.7);
+      transition: color 0.2s linear;
+
+      &::after {
+        content: '';
+        background-color: $white;
+        position: absolute;
+        height: 2px;
+        width: 0;
+        left: -0.1rem;
+        bottom: -0.25rem;
+        transition: width 0.2s linear;
+      }
+
+      &.active {
+        color: $white;
+
+        &::after {
+          width: calc(100% + 0.25rem);
+        }
+      }
+
+      &:hover {
+        color: $white;
+      }
+    }
+  }
+}
 
 .section.no-h-padding {
   padding: 3rem 0;
@@ -56,38 +90,5 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-}
-
-.mode-select {
-  line-height: 2.25rem;
-  margin-bottom: 3rem;
-}
-
-.developer {
-  padding: 0 1.5rem;
-}
-
-.designer {
-  position: relative;
-  background-color: $dark;
-
-  & > .column {
-    margin-top: 0;
-    margin-bottom: 0;
-    padding-top: 0;
-    padding-bottom: 0;
-  }
-  .details {
-    &.active {
-      @include mobile {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        padding: 0;
-      }
-    }
-  }
 }
 </style>
