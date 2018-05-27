16a04e2404efe3e113118e65909b8c2634196360
LokeCarlsson <loke.carlsson.lc@gmail.com>
2018-05-27 20:26:55 +0200

    Included port message on start when using prod server script

--- build/prod-server.js
+++ build/prod-server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || config.build.port;
 
 const app = express();
 
+console.log(`App listening on port ${port}`)
+
 // serve pure static assets
 const staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory);
 app.use(staticPath, express.static(path.resolve(__dirname, '..', 'dist/static')));
