5446fc8013b46d514f27ebcb22fbac8363d99070
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-01-13 13:24:53 +0100

    Update

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -1,30 +1,23 @@
 <template>
-    <div class="projects">
-        <div class="project" v-for="item in projects" :key="item.node.id">
-            <g-link :to="item.node.path" class="project-link">
-            <g-image
-                :src="item.node.thumbnail"
-                :alt="item.node.title"
-                class="thumbnail"
-            />
-            <h3 class="project-title">{{ item.node.title }}</h3>
-            <div class="categories">
-                <span class="category" v-for="(item, index) in item.node.categories" :key="index">{{ item }}</span>
-            </div>
-            </g-link>
-        </div>
+  <div class="projects">
+    <div class="project" v-for="item in projects" :key="item.node.id">
+      <g-link :to="item.node.path" class="project-link">
+        <g-image :src="item.node.thumbnail" :alt="item.node.title" class="thumbnail"/>
+        <h3 class="project-title">{{ item.node.title }}</h3>
+      </g-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        projects: {
-            type: Array,
-            required: true
-        }
+  props: {
+    projects: {
+      type: Array,
+      required: true
     }
-}
+  }
+};
 </script>
 
 <style scoped>
@@ -44,7 +37,7 @@ export default {
   height: 560px;
   object-fit: cover;
   transition: all 0.15s ease;
-  box-shadow: 0 0 40px -20px rgba(0,0,0,0.25);
+  box-shadow: 0 0 40px -20px rgba(0, 0, 0, 0.25);
 }
 .project-title {
   font-size: 1rem;
@@ -63,16 +56,15 @@ export default {
 }
 .project:hover .thumbnail {
   transform: scale(1.02);
-  box-shadow: 0 20px 40px -20px rgba(0,0,0,0.25);
+  box-shadow: 0 20px 40px -20px rgba(0, 0, 0, 0.25);
 }
 
 @media (min-width: 920px) {
   .project {
     grid-column: auto / span 1;
   }
-  .project:nth-child(3n+1) {
+  .project:nth-child(3n + 1) {
     grid-column: auto / span 2;
   }
 }
-
 </style>
