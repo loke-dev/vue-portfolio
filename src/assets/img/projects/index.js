39b11fad864af168290ac121f4e622f6e4317e40
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-01-13 22:55:25 +0100

    Add lots of assets

---
+++ src/assets/img/projects/index.js
@@ -0,0 +1,10 @@
+export { default as palceholder } from './placeholder.jpg';
+export { default as buyone } from './buyone.png';
+export { default as carleudd } from './carleudd.png';
+export { default as gitgud } from './gitgud.png';
+export { default as jekyll } from './jekyll.png';
+export { default as lillafro } from './lillafro.png';
+export { default as pwd } from './pwd.png';
+export { default as quiz } from './quiz.png';
+export { default as snippet } from './snippet.png';
+export { default as searchengine } from './searchengine.png';
