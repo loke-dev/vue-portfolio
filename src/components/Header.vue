77a088a3183edb12bd199ad3fedca3eeaa74f80c
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-01-13 21:59:40 +0100

    Update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,83 +1,76 @@
 <template>
-    <header class="header" :class="{sticky: $route.path === '/' || $route.path.includes('/projects/')}">
-        <div class="container">
-            <div class="left">
-                <g-link :to="{ name: 'home' }" class="home-link">
-                    <img 
-                        src="../../static/logo.svg"
-                        :alt="settings.site_name" 
-                        class="logo"
-                    />
-                </g-link>
-            </div>
-            <nav class="nav right">
-                <g-link class="nav__link" :to="{ name: 'journal' }">Journal</g-link>
-                <g-link class="nav__link" :to="{ name: 'contact' }">Say Hi!</g-link>
-            </nav>
-        </div>
-    </header>
+  <header class="header" :class="{sticky: $route.path === '/'}">
+    <div class="container">
+      <div class="nav left">
+        <g-link class="nav__link" :to="{ name: 'home' }">Home</g-link>
+      </div>
+      <nav class="nav right">
+        <g-link class="nav__link" :to="{ name: 'journal' }">Blog</g-link>
+        <g-link class="nav__link" :to="{ name: 'contact' }">Contact</g-link>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   data() {
     return {
-        logo: require("../../static/logo.svg"),
-        settings: require("../../data/theme.json")
-    }
+      settings: require("../../data/theme.json")
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 .header {
-    position: relative;
-    height: 6rem;
-    z-index: 10;
+  position: relative;
+  height: 5rem;
+  z-index: 10;
 }
 .header.sticky {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
 }
 .header > .container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
 }
 .home-link {
-    text-decoration: none;
+  text-decoration: none;
 }
 .logo {
-    height: 1.5rem;
+  height: 1.5rem;
 }
 .site-name {
-    font-size: 0.9rem;
-    font-weight: 700;
-    letter-spacing: 0.05em;
-    text-decoration: none;
-    text-transform: uppercase;   
+  font-size: 0.9rem;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  text-decoration: none;
+  text-transform: uppercase;
 }
 .nav > * {
-    font-size: 0.9rem;
-    font-weight: 600;
-    text-decoration: none;
-    margin-top: 4px;
-    margin-right: 3rem;
-    padding-bottom: 4px;
-    border-bottom: 1px solid;
-    border-color: transparent;
-    transition: border 0.15s;
+  font-size: 0.9rem;
+  font-weight: 600;
+  text-decoration: none;
+  margin-top: 4px;
+  margin-right: 3rem;
+  padding-bottom: 4px;
+  border-bottom: 1px solid;
+  border-color: transparent;
+  transition: border 0.15s;
 }
 .nav > *:last-of-type {
-    margin: 0;
+  margin: 0;
 }
 .nav > *:hover {
-    border-color: inherit;
+  border-color: inherit;
 }
-.nav > .active {
-    border-color: inherit;
+.nav > .active--exact {
+  border-color: inherit;
 }
 </style>
