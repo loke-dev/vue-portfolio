f6617c0da9f7cf4def466983e8185ed3982b4676
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-01-13 15:30:42 +0100

    Add success

---
+++ src/pages/Success.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="container">
+      <div class="success-hero">
+        <h1 class="success-header">Success!</h1>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
