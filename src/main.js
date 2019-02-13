2e8b2f4ec591ed8e85a6ec315591c400578cecb2
LokeCarlsson <carlssonloke@gmail.com>
2019-02-13 11:45:17 +0100

    Add bulma css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import "@/assets/code-highlight.css"
 
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
+  head.link.push({
+    rel: 'stylesheet',
+    href: '//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css'
+  })
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
