e41dcbbdcc4029a4cb5d9f899c16d78fdd22efe6
LokeCarlsson <carlssonloke@gmail.com>
2019-03-02 21:19:38 +0100

    Add fontawesome

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import settings from "../data/theme.json";
 
 import "@/assets/code-highlight.css"
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faGithub, faTwitter, faFacebook, faLinkedin, )
+
 export default function(Vue, { head }) {
+  Vue.component('font-awesome', FontAwesomeIcon)
   Vue.component("Layout", DefaultLayout);
   head.link.push({
     rel: 'stylesheet',
