e1984126569501aead50122125cbcd0f68fabcbe
LokeCarlsson <carlssonloke@gmail.com>
2019-03-02 22:15:13 +0100

    Add about page

---
+++ src/pages/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <div class="container">
+      <div class="contact-header">
+        <h1 class="contact-title">Hey there!</h1>
+        <p>dasdas</p>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+.contact-header
+  padding: 2rem 0 4rem 0
+
+.contact-title
+  font-size: 4rem
+  margin: 0 0 4rem 0
+  padding: 0
+</style>
+
