7673d424d150cad232b7d3dc4e54c04fe9980f6d
LokeCarlsson <carlssonloke@gmail.com>
2019-03-02 22:14:55 +0100

    Add styling

--- src/components/Skills/index.vue
+++ src/components/Skills/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="skills" class="section">
-    <div class="block has-text-centered">
+    <div class="title-section block has-text-centered">
       <h2 class="title is-2">Skills</h2>
       <h5 class="subtitle is-5">Technologies I work in most frequently.</h5>
     </div>
@@ -40,15 +40,22 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-  .columns
-    justify-content: center
+.section
+  padding-top: 75px
+  padding-bottom: 75px
 
-  .skill img
-    padding: 0.5em
-    margin: 0 auto
-    max-width: 110px
-    transition: all 0.15s ease-in
-    &:hover
-      transform: scale(1.1)
-      cursor: pointer
+.title-section
+  padding-bottom: 20px
+
+.columns
+  justify-content: center
+
+.skill img
+  padding: 0.5em
+  margin: 0 auto
+  max-width: 110px
+  transition: all 0.15s ease-in
+  &:hover
+    transform: scale(1.1)
+    cursor: pointer
 </style>
