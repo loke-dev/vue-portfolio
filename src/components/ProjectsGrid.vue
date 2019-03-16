99cfebf3122362ee7d9f5f08198389dcb4d78d17
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-03-16 14:00:52 +0100

    Update styling

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-  <div class="block has-text-centered">
-    <h2 class="title is-2">Projects</h2>
-    <h5 class="subtitle is-5">Web application and sites I have coded and designed.</h5>
+  <div class="title-section">
+    <h2 class="title">Projects</h2>
+    <h5 class="subtitle">Web application and sites I have coded and designed.</h5>
   </div>
   <div class="projects">
     <div class="project" v-for="item in projects" :key="item.node.id">
@@ -26,51 +26,69 @@ export default {
 };
 </script>
 
-<style scoped>
-.projects {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 4rem;
-}
-.project {
-  grid-column: auto / span 2;
-  text-align: center;
-}
-.project-link {
-  text-decoration: none;
-}
-.thumbnail {
-  height: 500px;
-  object-fit: cover;
-  transition: all 0.15s ease;
-  box-shadow: 0 0 40px -20px rgba(0, 0, 0, 0.25);
-}
-.project-title {
-  font-size: 1rem;
-  color: var(--color-contrast);
-  margin: 2rem 0 1rem 0;
-}
-.categories {
-  font-size: 0.8rem;
-  color: var(--color-contrast-1);
-}
-.category {
-  margin-right: 0.8rem;
-}
-.category:last-of-type {
-  margin: 0;
-}
-.project:hover .thumbnail {
-  transform: scale(1.02);
-  box-shadow: 0 20px 40px -20px rgba(0, 0, 0, 0.25);
-}
+<style lang="sass" scoped>
+.title-section
+  padding-bottom: 50px
+  text-align: center
 
-@media (min-width: 920px) {
-  .project {
-    grid-column: auto / span 1;
-  }
-  .project:nth-child(3n + 1) {
-    grid-column: auto / span 2;
-  }
-}
+.title
+  color: #363636
+  font-size: 2.5rem
+  font-weight: 600
+  line-height: 1.125
+  margin: 0
+
+.subtitle
+  color: #4a4a4a
+  font-size: 1.25rem
+  font-weight: 400
+  line-height: 1.25
+  word-break: break-word
+  margin: 0
+
+.projects
+  display: grid
+  grid-template-columns: 1fr 1fr
+  grid-gap: 4rem
+
+.project
+  grid-column: auto / span 2
+  text-align: center
+
+.project-link
+  text-decoration: none
+
+.thumbnail
+  height: 500px
+  object-fit: cover
+  transition: all 0.15s ease
+  box-shadow: 0 0 40px -20px rgba(0, 0, 0, 0.25)
+
+.project-title
+  font-size: 1rem
+  color: var(--color-contrast)
+  margin: 2rem 0 1rem 0
+
+.categories 
+  font-size: 0.8rem
+  color: var(--color-contrast-1)
+
+.category
+  margin-right: 0.8rem
+
+.category:last-of-type
+  margin: 0
+
+.project:hover .thumbnail
+  transform: scale(1.02)
+  box-shadow: 0 20px 40px -20px rgba(0, 0, 0, 0.25)
+
+
+@media (min-width: 920px)
+  .project
+    grid-column: auto / span 1
+  
+  .project:nth-child(3n + 1)
+    grid-column: auto / span 2
+
 </style>
--- src/components/Skills/index.vue
+++ src/components/Skills/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <section id="skills" class="section">
-    <div class="title-section block has-text-centered">
-      <h2 class="title is-2">Skills</h2>
-      <h5 class="subtitle is-5">Technologies I work in most frequently.</h5>
+  <section id="skills-wrapper" class="section">
+    <div class="title-section">
+      <h2 class="title">Skills</h2>
+      <h5 class="subtitle">Technologies I work in most frequently.</h5>
     </div>
-    <div class="skills columns is-multiline is-mobile">
-      <div class="column is-half-mobile is-one-third-tablet is-one-quarter-desktop"
+    <div class="skills">
+      <div class="skill"
         v-for="(skill, index) in skills"
         :key="index">
-        <div class="skill">
-          <a class="image"
-            target="_blank"
-            rel="noopener"
-            :href="skill.url"
-            :title="skill.name">
-              <img :src="skill.image" :alt="skill.name">
-              <div class="preloader" slot="placeholder" />
-          </a>
-        </div>
+        <a class="image"
+          target="_blank"
+          rel="noopener"
+          :href="skill.url"
+          :title="skill.name">
+            <img :src="skill.image" :alt="skill.name">
+            <div class="preloader" slot="placeholder" />
+        </a>
       </div>
     </div>
   </section>
@@ -45,12 +43,41 @@ export default {
   padding-bottom: 75px
 
 .title-section
-  padding-bottom: 20px
+  padding-bottom: 50px
+  text-align: center
 
-.columns
-  justify-content: center
+.title
+  color: #363636
+  font-size: 2.5rem
+  font-weight: 600
+  line-height: 1.125
+  margin: 0
+
+.subtitle
+  color: #4a4a4a
+  font-size: 1.25rem
+  font-weight: 400
+  line-height: 1.25
+  word-break: break-word
+  margin: 0
+
+.skills
+  display: flex
+  flex-wrap: wrap
+
+.skill
+  flex: none
+  width: 25%
+  padding: 20px
+  @media screen and (max-width: 720px)
+    flex: none
+    width: 33.3333%
+  @media screen and (max-width: 430px)
+    flex: none
+    width: 50%
 
 .skill img
+  display: flex
   padding: 0.5em
   margin: 0 auto
   max-width: 110px
