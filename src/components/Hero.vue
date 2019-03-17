c5d70afd72f19d4faa39434c36b47878014f92bd
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-03-17 16:32:15 +0100

    Use title instead of subtitle and fix styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hero">
     <img class="hero-image" alt="hero logo" src="../assets/img/logo.png" />
-    <h2 class="hero-subtitle" v-html="settings.hero_subtitle" />
+    <h2 class="hero-title" v-html="settings.hero_title" />
   </div>
 </template>
 
@@ -15,42 +15,32 @@ export default {
 }
 </script>
 
-<style>
-.hero {
-  text-align: center;
-  width: 100vw!important;
-  margin: 0 auto;
-  margin-bottom: 50px;
-  padding: 16rem 0 8rem 0;
-  position: relative;
-  justify-content: inherit!important;
-  height: 100vh;
-  background: linear-gradient(to right, #2f454e, #203A43, #11252d);
-}
+<style lang="sass">
+.hero
+  text-align: center
+  width: 100vw
+  margin: 0 auto
+  margin-bottom: 50px
+  padding: 16rem 0 8rem 0
+  position: relative
+  justify-content: inherit!important
+  height: 100vh
+  background: linear-gradient(to right, #2f454e, #203A43, #11252d)
 
-.hero-title {
-  font-size: 3rem;
-  font-weight: 700;
-  padding: 0;
-  margin: 0 0 2rem 0;
-}
-.hero-title p,
-.hero-subtitle p {
-  margin: 0;
-  padding: 0;
-}
-.hero-subtitle {
-  font-size: 1.15em;
-  font-weight: 400;
-  color: white;
-  line-height: 1.68;
-  padding: 40px 20px;
-  opacity: 0.6;
-}
-.hero-image {
-  padding-bottom: 30px;
-  max-width: 800px;
-  width: 90vw;
-  margin: 0 auto;
-}
+.hero-title 
+  margin: 0
+  padding: 0
+  font-size: 1.5em
+  font-weight: 200
+  color: white
+  line-height: 1.68
+  padding: 40px 20px
+  opacity: 0.6
+
+.hero-image
+  padding-bottom: 30px
+  max-width: 800px
+  width: 90vw
+  margin: 0 auto
+
 </style>
