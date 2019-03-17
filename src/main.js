9cc98b843f1022206d9dc5f9a91285301711d3e3
LokeCarlsson <loke.carlsson.lc@gmail.com>
2019-03-17 19:56:26 +0100

    Add fontawesome package

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import "@/assets/code-highlight.css"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faGithub, faTwitter, faFacebook, faLinkedin, )
+library.add(faGithub, faTwitter, faFacebook, faLinkedin, faEnvelope)
 
 export default function(Vue, { head }) {
   Vue.component('font-awesome', FontAwesomeIcon)
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
-  };
+  }
 }
