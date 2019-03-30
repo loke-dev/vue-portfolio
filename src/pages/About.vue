2571d384114af343b98dc980b58e79cc97b8462b
LokeCarlsson <carlssonloke@gmail.com>
2019-03-30 17:38:01 +0100

    Rewording

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,18 +8,18 @@
         <img class="profile-image" src="../assets/img/profile.png">
         <div class="about-text">
           <p>
-            As a frontend developer, Loke Carlsson focuses on solving problems. 
-            He values his own word, putting an emphasis on loyalty and honest with his clients and fellow team members. 
-            Passionate about the JAM stack, he strives for intuitiveness in all the frontend work that he does, 
-            doing everything that he can to make users' experiences smooth, easy, and comfortable.
+            As a frontend developer, I focus on solving problems. 
+            Putting an emphasis on loyalty and honesty with clients and fellow team members. 
+            Passionate about the JAM stack, I strives for intuitiveness in all the work that I do, 
+            doing everything that I can to make users' experiences smooth, easy, and comfortable.
           </p>
           <p>
-            Loke has worked in both Vue.js and React.js, 
-            and he uses these packages to the best of his ability in order to bring his and his clients' UI visions to life. 
-            His preferred CSS preprocesor is Sass, and his ability to style the pages and platforms that he develops is noteworthy, 
-            his technical skills complemented nicely by his fashionable eye. 
-            Considering at all times what will make his interfaces attractive and what will make them efficient, 
-            he develops in a way that plays to users' expectations – while also surprising them pleasantly with unique and different ideas.
+            I work with both Vue.js and React.js regularly,
+            and try to use these tools to the best of my ability in order to bring my clients' UI visions to life. 
+            My preferred CSS preprocesor is Sass, and my ability to style the pages and platforms that I develops is noteworthy, 
+            my technical skills complemented nicely by my fashionable eye. 
+            Considering at all times what will make interfaces attractive and what will make them efficient, 
+            I try to develop in a way that plays to users' expectations – while also surprising them pleasantly with unique and different ideas.
           </p>
         </div>
       </div>
